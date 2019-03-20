5b67b8cd5b944760a5c2a1c540ffd48cfda1f277
jordi <jordi.herrera@enti.cat>
2019-03-20 13:04:56 +0100

    Inicio enemies

---
+++ NEOM/Assets/Scripts/Enemies.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemies : MonoBehaviour {
+
+    public GameObject enemy;
+    public int health = 100;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        if (health <= 0)
+        {
+            Destroy(enemy);
+        }
+    }
+}
---
+++ NEOM/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour {
+
+    int force = -40;
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("enemyTrigger"))
+        {
+            force *= -1;
+            transform.Rotate(0f, 180f, 0f);
+        }
+    }
+
+    void Update()
+    {
+        
+        GetComponent<Rigidbody2D>().AddForce(new Vector2(force, 0));
+    }
+}
