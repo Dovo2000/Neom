56160173d2426e127bdf0b3169f5f76875efaec4
Eduard Monera <eduard.monera@enti.cat>
2019-03-27 11:35:43 +0100

    timer y hitbox suelo

---
+++ NEOM/Assets/Scripts/respawntest.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class respawntest : MonoBehaviour {
+
+    [SerializeField] Transform Respawnpoint;
+
+    
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.CompareTag("Player"))
+            collision.transform.position = Respawnpoint.position;
+    }
+
+
+}
---
+++ NEOM/Assets/Scripts/timer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class timer : MonoBehaviour {
+
+    float tiempo = 0f;
+    float inicio = 1f;
+
+    [SerializeField] Text countdownText;
+    [SerializeField] Transform Respawnpoint;
+
+    private void Start()
+    {
+        tiempo = inicio;
+    }
+
+    private void Update()
+    {
+        tiempo -= 1 * Time.deltaTime;
+        countdownText.text = tiempo.ToString("0");
+
+        if(tiempo <= 0 )
+        {
+            tiempo = 0;
+
+            transform.position = Respawnpoint.position;
+
+        }
+    }
+
+    
+}
