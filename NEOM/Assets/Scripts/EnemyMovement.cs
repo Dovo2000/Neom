9a0d17b17ad1193cf0ad6ccd899e621b7f8ac9af
jordi <jordihc2000@hotmail.com>
2019-03-30 19:49:40 +0100

    Enemy Shoot
    Disparos de enemigos y creacion de script con la info el player

--- NEOM/Assets/Scripts/EnemyMovement.cs
+++ NEOM/Assets/Scripts/EnemyMovement.cs
@@ -6,12 +6,22 @@ public class EnemyMovement : MonoBehaviour {
     public float speed = 2;
     public int detectionRate = 4;
     bool detection = false;
-    int force = -40;
+    int force = 40;
     private Transform target;
+    public Transform firePoint;
+    public LineRenderer lineRenderer;
+    public int damage;
+    private float timeBtwShoots;
+    public float startTimeBtwShoots;
+    private float timeLine;
+    public float startTimeLine;
 
     void Start()
     {
         target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+
+        timeBtwShoots = startTimeBtwShoots;
+        timeLine = startTimeLine;
     }
 
     void OnTriggerEnter2D(Collider2D collision)
@@ -29,7 +39,7 @@ public class EnemyMovement : MonoBehaviour {
         {
             detection = true;
         }
-        
+
         if (detection == false)
         {
             GetComponent<Rigidbody2D>().AddForce(new Vector2(force, 0));
@@ -37,11 +47,52 @@ public class EnemyMovement : MonoBehaviour {
         }
         else
         {
-            
+
+            StartCoroutine(enemyShoot());
+
+
             if ((Vector2.Distance(transform.position, target.position) > detectionRate))
             {
                 detection = false;
             }
+        }
+    }
+
+    IEnumerator enemyShoot()
+    {
+        if (timeBtwShoots <= 0)
+        {
+            RaycastHit2D hitInfo = Physics2D.Raycast(firePoint.position, firePoint.right);
+
+            if (hitInfo)
+            {
+                Player player = hitInfo.transform.GetComponent<Player>();
+
+                if (player != null)
+                {
+                    player.takeDamage(damage);
+                }
+                lineRenderer.SetPosition(0, firePoint.position);
+                lineRenderer.SetPosition(1, hitInfo.point);
+            }
+            else
+            {
+                lineRenderer.SetPosition(0, firePoint.position);
+                lineRenderer.SetPosition(1, firePoint.position + firePoint.right * 100);
+            }
+
+            lineRenderer.enabled = true;
+
+            yield return 1;
+
+            lineRenderer.enabled = false;
+
+            timeBtwShoots = startTimeBtwShoots;
+        }
+
+        else if (timeBtwShoots > 0)
+        {
+            timeBtwShoots -= Time.deltaTime;
         }
     }
 }
---
+++ NEOM/Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    
+    public int health;
+
+    
+    public void takeDamage(int damage)
+    {
+        health -= damage;
+        if (health <= 0)
+        {
+            //gameOver();
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
