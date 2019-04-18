28856d114f969f1bda7b6242080cb3caa1557599
Eduard <eduard.monera@enti.cat>
2019-04-18 18:32:40 +0200

    prueba escalera 1

---
+++ NEOM/Assets/escaleras.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class escaleras : MonoBehaviour {
+
+    public bool inside = false; 
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+
+            inside = !inside;
+        }
+
+    }
+    void update()
+    {
+        if (inside == true && Input.GetKey("W"))
+        {
+         
+        }
+    }
+}
