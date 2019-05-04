e6c7be77109f59b47ad6b3225c69edb0dec4b667
Eduard <eduard.monera@enti.cat>
2019-05-04 12:56:03 +0200

    enemigos parpadeo

--- NEOM/Assets/Scripts/Enemies.cs
+++ NEOM/Assets/Scripts/Enemies.cs
@@ -10,9 +10,13 @@ public class Enemies : MonoBehaviour {
     public int health = 100;
     public float deathCounter = 0;
 
+    public Color colorToTurnTo = Color.red;
+    public Color colorByDefault = Color.white;
+
     public void TakeDamage(int damage)
     {
         health -= damage;
+        StartCoroutine(Flash());
         if (health <= 0)
         {
 
@@ -20,5 +24,15 @@ public class Enemies : MonoBehaviour {
             deathCounter++;
         }
     }
-    
+
+    IEnumerator Flash()
+    {
+        for (int n = 0; n < 2; n++)
+        {
+            GetComponent<Renderer>().material.color = colorToTurnTo;
+            yield return new WaitForSeconds(0.1f);
+            GetComponent<Renderer>().material.color = colorByDefault;
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
 }
