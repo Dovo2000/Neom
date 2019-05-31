b6ddaefba2cfd084ac4906c20cf668247ecd5e5e
Eduard <eduard.monera@enti.cat>
2019-05-31 18:26:34 +0200

    menu settings

--- NEOM/Assets/Scripts/MainMenu.cs
+++ NEOM/Assets/Scripts/MainMenu.cs
@@ -22,6 +22,11 @@ public class MainMenu : MonoBehaviour {
         SceneManager.LoadScene("Creditos");
     }
 
+    public void Settings()
+    {
+        SceneManager.LoadScene("Settings");
+    }
+
     public void ReturnMenu()
     {
         SceneManager.LoadScene("Menu");
