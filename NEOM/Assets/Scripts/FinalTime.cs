7b5c07adbd62d5cb772c290cab8534adffe47a10
David Martín <david.martin@enti.cat>
2019-06-02 19:01:14 +0200

    Final

--- NEOM/Assets/Scripts/FinalTime.cs
+++ NEOM/Assets/Scripts/FinalTime.cs
@@ -12,10 +12,14 @@ public class FinalTime : MonoBehaviour {
     public float tiempoFinal;
 
     [SerializeField] Text Puntuacion;
+    [SerializeField] Text Chascarrillo;
 
     // Update is called once per frame
     void Update () {
         tiempoFinal = final - (inicio - (finalPausa - inicioPausa));
         Puntuacion.text = tiempoFinal.ToString();
-	}
+        if (tiempoFinal > 45) Chascarrillo.text = "That was bad!";
+        else if (tiempoFinal > 35 && tiempoFinal <= 45) Chascarrillo.text = "You can do it better!";
+        else if (tiempoFinal <= 35) Chascarrillo.text = "Perfect!";
+    }
 }
