4b1dbec3e1348a4a23bec130669c2b766df46f7f
jordi <jordihc2000@hotmail.com>
2019-06-02 16:58:57 +0200

    PauseMenu

---
+++ NEOM/Assets/PauseControl.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseControl : MonoBehaviour {
+    timer time;
+    MovimientoPlayer player;
+    EnemyMovement enemy;
+
+    public void Resume()
+    {
+        
+    }
+
+    public void Settings()
+    {
+
+    }
+
+    public void Quit()
+    {
+        
+    }
+}
