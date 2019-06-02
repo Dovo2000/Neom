823eb63ac4d47c0e5e6070f56fd03499346c7130
David Martín <david.martin@enti.cat>
2019-06-02 18:19:21 +0200

    Pausa final

--- NEOM/Assets/PauseControl.cs
+++ NEOM/Assets/PauseControl.cs
@@ -1,24 +1,62 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class PauseControl : MonoBehaviour {
-    timer time;
-    MovimientoPlayer player;
-    EnemyMovement enemy;
+
+    Canvas canvas;
+    public Canvas canvasOpciones;
+
+    void Start()
+    {
+        canvas = GetComponent<Canvas>();
+        canvasOpciones.enabled = false;
+        canvas.enabled = false;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            Pause();
+        }
+    }
+
+    public void Pause()
+    {
+        if (Time.timeScale == 1) FinalTime.inicioPausa = Time.realtimeSinceStartup;
+        else if (Time.timeScale == 0) FinalTime.finalPausa = Time.realtimeSinceStartup;
+        canvas.enabled = !canvas.enabled;
+        Time.timeScale = Time.timeScale == 0 ? 1 : 0;
+    }
 
     public void Resume()
     {
-        
+        FinalTime.finalPausa = Time.realtimeSinceStartup;
+        canvas.enabled = false;
+        Time.timeScale = 1f;
     }
 
-    public void Settings()
+    public void Menu()
     {
-
+        canvas.enabled = false;
+        Time.timeScale = 1;
+        FinalTime.inicioPausa = 0f;
+        FinalTime.finalPausa = 0f;
+        Musicafondo.instance.musicSource.pitch = 0.9f;
+        Musicafondo.instance.musicSource.volume = 0.2f;
+        Musicafondo.instance.musicSource.Play();
+        SceneManager.LoadScene("Menu");
     }
 
-    public void Quit()
+    public void Opciones()
     {
-        
+        canvasOpciones.enabled = true;
+    }
+
+    public void Back()
+    {
+        canvasOpciones.enabled = false;
     }
 }
--- NEOM/Assets/Scripts/FinalTime.cs
+++ NEOM/Assets/Scripts/FinalTime.cs
@@ -7,13 +7,15 @@ public class FinalTime : MonoBehaviour {
 
     public static float inicio;
     public static float final;
+    public static float inicioPausa;
+    public static float finalPausa;
     public float tiempoFinal;
 
     [SerializeField] Text Puntuacion;
 
     // Update is called once per frame
     void Update () {
-        tiempoFinal = final - inicio;
+        tiempoFinal = final - (inicio - (finalPausa - inicioPausa));
         Puntuacion.text = tiempoFinal.ToString();
 	}
 }
